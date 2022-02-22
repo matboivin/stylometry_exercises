8bd4a6e9ee390ff83337498270104080b901477e
Mathilde BOIVIN <mboivin@student.42.fr>
2022-02-22 19:43:10 +0100

    sort and store files

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
 #!/usr/bin/env python3
 
-articles_per_categories = {
-    "madison": [10, 14, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48],
-    "hamilton": [1, 6, 7, 8, 9, 11, 12, 13, 15, 16, 17, 21, 22, 23, 24,
-                 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 59, 60,
-                 61, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77,
-                 78, 79, 80, 81, 82, 83, 84, 85],
-    "jay": [2, 3, 4, 5],
-    "cowritten": [18, 19, 20],
-    "disputed": [49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 62, 63],
-    "specialcase": [64]
-}
-
-
 def combine_files(file_idx):
     """Combine the files matching the given indices
     
@@ -27,8 +14,27 @@ def combine_files(file_idx):
     return "\n".join(files)
 
 
+def sort_files_per_category():
+    idx_per_category = {
+        "madison": [10, 14, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48],
+        "hamilton": [1, 6, 7, 8, 9, 11, 12, 13, 15, 16, 17, 21, 22, 23, 24,
+                    25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 59, 60,
+                    61, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77,
+                    78, 79, 80, 81, 82, 83, 84, 85],
+        "jay": [2, 3, 4, 5],
+        "cowritten": [18, 19, 20],
+        "disputed": [49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 62, 63],
+        "specialcase": [64]
+    }
+    files_per_category = {}
+
+    for category,idx in idx_per_category.items():
+        files_per_category[category] = combine_files(idx)
+    return files_per_category
+
+
 def main():
-    combine_files(articles_per_categories["madison"]))
+    print(sort_files_per_category())
 
 
 if __name__ == "__main__":
